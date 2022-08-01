456cdcecc3836404cc16f68a6d9bcff9323def03
ruben.rica@soprasteria.com <ruben.rica@soprasteria.com>
2022-08-01 11:46:20 +0200

    feat: add view model

--- CryptoPlumber/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoPlumber/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,6 +6,7 @@ class HomeViewModel: ObservableObject {
     @Published var portfolioCoins: [Coin] = []
     
     init() {
+        // TODO: remove after Http service
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             self.allCoins.append(DeveloperPreview.instance.coin)
             self.portfolioCoins.append(DeveloperPreview.instance.coin)
